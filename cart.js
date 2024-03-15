74d6d0a84499783a8c4c351dd2165faf9fd7bada
sticktostack <rajdeep6153@gmail.com>
2024-03-16 02:36:32 +0530

    final commit

--- cart.js
+++ cart.js
@@ -1,19 +1,26 @@
+// showing the cart box after clicking on the carticon
 document.querySelector("#carticon").addEventListener("click", () => {
   document.querySelector(".cartsection").style.transform = "translateX(0%)";
 });
+// hiding the cart box after clicking on the carticon
 document.querySelector("#closebtn").addEventListener("click", () => {
   document.querySelector(".cartsection").style.transform = "translateX(100%)";
 });
-let cardContainer = document.querySelector(".card-container");
 
+// holding the values of the cart section 
+let cart = [];
+
+// loading all the products from an random API 
 function loadProducts(){
 fetch("https://dummyjson.com/products")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    let products = data.products;
+    let products = data.products; // holding the array
+
     products.forEach((product) => {
+      // looping over all the fetched products 
       let div = document.createElement("div");
       div.className = "cards";
       let img = document.createElement("img");
@@ -28,28 +35,49 @@ fetch("https://dummyjson.com/products")
       addBtn.id = "add";
       div.addEventListener('click',(e)=>{
         if(e.target === addBtn){
+          // showing the animation after clicking the add button 
           document.querySelector("#carticon").innerHTML = 'Added to cart'
           document.querySelector("#carticon").style.transform = 'translateX(30%)'
           document.querySelector("#carticon").style.fontSize = '26px'
           setTimeout(() => {
             document.querySelector("#carticon").style.fontSize = '45px'
           document.querySelector("#carticon").innerHTML = '<i class="ri-shopping-cart-2-fill"></i>'
-          }, 1000);
+          }, 500);
+          // animation ends here 
+
+          let card = e.target.closest('.cards') //holding the nearest div of the individual addBUtton 
+          let product = { // creating a object after clicking on the add button which holds all the data ofthe individual product
+            image : card.querySelector('img').src,
+            title: card.querySelector('h3').innerHTML,
+            price: card.querySelector('h4').innerHTML
+          };
+          // creating the cart section dynamically 
+          document.querySelector('.productsection').innerHTML += `
+          <div class="product">
+                <div class="left"><img src="${product.image}" ></div>
+                <div class="right">
+                    <h2>${product.title}</h2>
+                    <p>price : ${product.price}</p>
+                </div>
+            </div>
+          `
+          // pushing all the added products on the cart 
+          cart.push(product)
+          console.log(cart);
+          
         }else{
           console.log('no');
         }
       })
 
-
+      // appending all the dynamically created elements 
       div.appendChild(img);
       div.appendChild(title);
       div.appendChild(price);
       div.appendChild(addBtn);
-      cardContainer.appendChild(div);
+      document.querySelector(".card-container").appendChild(div);
 
     });
   });
 }
 loadProducts()
-
-let cart = [];
